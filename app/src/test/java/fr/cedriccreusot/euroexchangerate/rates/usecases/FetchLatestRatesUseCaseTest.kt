3824a1933536005611005f7b4bf65bae34b5c261
Cedric Creusot <cedric.creusot@gmail.com>
2020-02-09 19:06:35 +0100

    add second test

--- app/src/test/java/fr/cedriccreusot/euroexchangerate/rates/usecases/FetchLatestRatesUseCaseTest.kt
+++ app/src/test/java/fr/cedriccreusot/euroexchangerate/rates/usecases/FetchLatestRatesUseCaseTest.kt
@@ -3,18 +3,27 @@ package fr.cedriccreusot.euroexchangerate.rates.usecases
 import com.google.common.truth.Truth.assertThat
 import com.nhaarman.mockitokotlin2.given
 import com.nhaarman.mockitokotlin2.verify
+import fr.cedriccreusot.euroexchangerate.rates.usecases.models.Rate
 import fr.cedriccreusot.euroexchangerate.rates.usecases.repositories.GetRatesRepository
+import io.kotlintest.TestCase
 import io.kotlintest.specs.StringSpec
 import org.mockito.Mockito.mock
+import kotlin.math.exp
 
 class FetchLatestRatesUseCaseTest : StringSpec ({
 
-    """Given we're fetching the latest rate
-        |When the repository throw an Exception
-        |Then the use case should return that Exception""" {
-        val repository = mock(GetRatesRepository::class.java)
-        val usecase = FetchLatestRatesUseCaseImpl(repository)
+    lateinit var repository : GetRatesRepository
+    lateinit var usecase: FetchLatestRatesUseCase
 
+    fun setup() {
+        repository = mock(GetRatesRepository::class.java)
+        usecase = FetchLatestRatesUseCase.createUseCase(repository)
+    }
+
+    """Given we're fetching the latest rate
+        When the repository throw an Exception
+        Then the use case should return that Exception""" {
+        setup()
         given(repository.rates()).willThrow(Exception())
 
         val result = runCatching {
@@ -22,6 +31,24 @@ class FetchLatestRatesUseCaseTest : StringSpec ({
         }
 
         assertThat(result.exceptionOrNull()).isNotNull()
+        verify(repository).rates()
+    }
+
+    """Given we're fetching the latest rate
+        When the repository return a list of rates
+        Then the use case should return it""" {
+        setup()
+        val expected = listOf(
+            Rate("USD", 1.305.toBigDecimal()),
+            Rate("PNL", 1.305.toBigDecimal())
+        )
+
+        given(repository.rates()).willReturn(expected)
+
+        val result = usecase.invoke()
+
+        assertThat(result).isNotNull()
+        assertThat(result).isEqualTo(expected)
         verify(repository).rates()
     }
 })
